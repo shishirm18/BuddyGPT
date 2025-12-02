9ce4d6daf9b4344070e8941d61d0740a50a3a659
shishirm18 <shishirbittu1997@gmail.com>
2025-12-02 13:38:28 -0500

    Routes: Fetching and deleting single chat thread

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -32,4 +32,34 @@ router.get("/thread", async(req, res) => {
     }
 })
 
+// Get single threads
+router.get("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+    try {
+        const thread = await Thread.findOne({threadId});
+        if (!thread){
+            res.status(404).json({error: "Thread not found"});
+        }
+        res.json(thread.messages);
+    } catch(err) {
+        console.log(err);
+        res.send(500).json({error: "Failed to get the chat"});
+    }
+})
+
+// Delete single threads
+router.delete("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+    try {
+        const deletedThread = await Thread.findOneAndDelete({threadId});
+        if (!deletedThread){
+            res.status(404).json({error: "Thread not found"});
+        }
+        res.status(200).json({sucess: "Thread deleted successfully!"})
+    } catch(err) {
+        console.log(err);
+        res.send(500).json({error: "Failed to delete the thread"});
+    }
+})
+
 export default router;
