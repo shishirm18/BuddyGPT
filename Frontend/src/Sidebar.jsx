679629c5c126393a7501ab19dd58a69c20b98cb7
shishirm18 <shishirbittu1997@gmail.com>
2025-12-04 22:11:09 -0500

    frontend sidebar organizing

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -2,9 +2,27 @@ import "./Sidebar.css"
 
 function Sidebar() {
     return (
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+            <button className="logobtn">
+                <img src="src/assets/buddylogo.png" alt="buddy logo" className="logo"/>
+                <p className="appname">BuddyGPT</p>
+            </button>
+            
+            <button className="newchat-btn">
+                <span><i className="fa-solid fa-pen-to-square"></i></span>
+                <p>New Chat</p>
+            </button>
+            
+            <ul className="history">
+                <li>thread 1</li>
+                <li>thread 2</li>
+                <li>thread 3</li>
+            </ul>
+
+            <div className="sign">
+                <p>By Shishir</p>
+            </div>
+        </section> 
     )
 }
 
