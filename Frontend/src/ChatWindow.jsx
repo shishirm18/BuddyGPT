f3e64e17b66bf0ce9aeb64661d346aa630666566
shishirm18 <shishirbittu1997@gmail.com>
2025-12-05 14:25:46 -0500

    ChatWindow: Added navbar section

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,25 @@
 import "./ChatWindow.css"
+import Chats from "./Chats.jsx"
 
 function ChatWindow() {
     return (
-        <div>
-            ChatWindow
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>GPT Ver<i class="fa-solid fa-angle-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            <Chats></Chats>
+            <div className="chatInput">
+                <div className="userInput">
+                    <input type="text" placeholder="Ask anything"/>
+                    <div id="submit"><i class="fa-solid fa-arrow-right"></i></div>
+                </div>
+                <p className="info">
+                    BuddyGPT can make mistakes. Check important info. See Cookie Preferences.
+                </p>
+            </div>
         </div>
     )
 }
