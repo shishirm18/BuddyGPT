fd9f48dae4139903df12e4f3fdd1cecabe4dd2c7
shishirm18 <shishirbittu1997@gmail.com>
2025-12-05 14:22:53 -0500

    Added MyContext and made small changes to App comp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css';
 import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
+import { MyContext } from './MyContext.jsx';
 
 function App() {
+  const providerValues = {}; // we'll be passing down the values from this object. Wrap the components with the Provider where we'll pass all the values
 
   return (
-    <>
-      <Sidebar></Sidebar>
-      <ChatWindow></ChatWindow>
-    </>
+    <div className='app'>
+      <MyContext.Provider values = {providerValues}> 
+        <Sidebar></Sidebar>
+        <ChatWindow></ChatWindow>
+      </MyContext.Provider>
+    </div>
   )
 }
 
-export default App
+export default App;
