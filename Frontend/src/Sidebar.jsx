283f0f42ee1e4aae2d89cd1e4347b57e85c03611
shishirm18 <shishirbittu1997@gmail.com>
2025-12-04 23:31:30 -0500

    sidebar basic setup done

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -3,24 +3,25 @@ import "./Sidebar.css"
 function Sidebar() {
     return (
         <section className="sidebar">
-            <button className="logobtn">
-                <img src="src/assets/buddylogo.png" alt="buddy logo" className="logo"/>
-                <p className="appname">BuddyGPT</p>
-            </button>
-            
-            <button className="newchat-btn">
-                <span><i className="fa-solid fa-pen-to-square"></i></span>
-                <p>New Chat</p>
-            </button>
-            
-            <ul className="history">
-                <li>thread 1</li>
-                <li>thread 2</li>
-                <li>thread 3</li>
-            </ul>
-
+            <div>
+                <button className="logobtn">
+                    <img src="src/assets/buddylogo.png" alt="buddy logo" className="logo"/>
+                    <p className="appname">BuddyGPT</p>
+                </button>
+                
+                <button className="newchat-btn">
+                    <span><i className="fa-solid fa-pen-to-square"></i></span>
+                    <p>New Chat</p>
+                </button>
+                
+                <ul className="history">
+                    <li>thread 1</li>
+                    <li>thread 2</li>
+                    <li>thread 3</li>
+                </ul>
+            </div>
             <div className="sign">
-                <p>By Shishir</p>
+                <p>By Shishir &#10024;</p>
             </div>
         </section> 
     )
