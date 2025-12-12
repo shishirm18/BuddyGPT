8db28523c0c1186ea5586caf8c2465723a999398
shishirm18 <shishirbittu1997@gmail.com>
2025-12-11 23:37:22 -0500

    Formatted the gpt reply

--- Frontend/src/Chats.jsx
+++ Frontend/src/Chats.jsx
@@ -1,6 +1,9 @@
 import "./Chats.css"
 import { useContext } from "react";
 import { MyContext } from "./MyContext";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github.css";
 
 function Chats() {
     const { newChat, prevChats } = useContext(MyContext);
@@ -15,7 +18,8 @@ function Chats() {
                         {
                             chat.role === "user"? 
                             <p className="userMessage">{chat.content}</p> : 
-                            <p className="gptMessage">{chat.content}</p>
+                            // <p className="gptMessage">{chat.content}</p>
+                            <ReactMarkdown rehypePlugins={rehypeHighlight}>{chat.content}</ReactMarkdown>
                         }
                     </div>
                 )
