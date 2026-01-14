f42a9b6d178eafbbcd5944a380eed5e1e0a08bc9
shishirm18 <shishirbittu1997@gmail.com>
2026-01-13 22:00:01 -0500

    Testing for deployment issue

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", chatRoutes);
+app.use("/", chatRoutes);
 
 const connectDB = async() => {
   try {
